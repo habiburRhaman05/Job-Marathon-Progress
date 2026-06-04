5f2e5318c26ab3a1e49124fa77f4fe8359330951
habiburRhaman05 <devhabib2005@gmail.com>
2026-06-04 20:32:09 +0600

    sloved secend problem - day-2

--- Problem Slove/Day-1 04-06-26/index.js
+++ Problem Slove/Day-1 04-06-26/index.js
@@ -16,3 +16,26 @@ function reverseString(str){
 };
 
 console.log(reverseString("developer"));
+
+
+// Problem 7: Count Vowels in a String  [Easy]
+// Description: Write a function countVowels(str) that counts and returns the number of vowels (a, e, i, o, u) in a string.
+// Example:
+// Input: 'hello'   → Output: 2Input: 'javascript' → Output: 3
+// Hint: Use a loop or match() with a regular expression.
+
+
+function countVowels(str){
+    let vowels = ["a","e","i","o","u"];
+    let count =0;
+  for (let i = 0; i < str.length; i++) {
+    
+    if(vowels.includes(str[i])){
+      count++
+    }
+    
+  }
+  return count
+
+};
+console.log(countVowels("javascript"));
