a858827c3d6aa6f733ecf09cfc7d5b3307accb58
habiburRhaman05 <devhabib2005@gmail.com>
2026-06-11 22:33:33 +0600

    sloved event-emitter problem

--- Problem Slove/Day-7/index.js
+++ Problem Slove/Day-7/index.js
@@ -129,3 +129,32 @@ console.log(original.address.city); // Dhaka
 console.log(original.skills[0]);    // React
   console.log(copied);
   
+
+
+//   Problem 34: Event Emitter  [Medium]
+// Description: Build a simple EventEmitter class with on(event, listener), emit(event, ...args), and off(event, listener) methods.
+// Example:
+// const emitter = new EventEmitter();emitter.on('greet', name => console.log('Hello ' + name));emitter.emit('greet', 'Sara'); // Hello Sara
+// Hint: Store listeners in an object where keys are event names and values are arrays of functions.
+
+class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  on(event, listener) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+
+    this.events[event].push(listener);
+  }
+
+  emit(event, ...args) {
+    if (!this.events[event]) return;
+
+    this.events[event].forEach(listener => {
+      listener(...args);
+    });
+  }
+}
